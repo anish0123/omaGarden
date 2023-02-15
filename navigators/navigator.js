f88410139149e6a88c8f173dfb93b6610930d0aa
anish0123 <anishmhn@hotmail.com>
2023-02-15 11:31:50 +0200

    created homepage, tab navigator, stack screen and added animation at tab navigator

---
+++ navigators/navigator.js
@@ -0,0 +1,59 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Main from '../views/Main';
+import Upload from '../views/Upload';
+import LottieIcons from '../components/LottieIcons';
+import {MainContext} from '../contexts/MainContext';
+import {useContext} from 'react';
+import Login from '../views/Login';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const TabScreen = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          return <LottieIcons iconName={route.name} focused={focused} />;
+        },
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarItemStyle: {padding: 4},
+      })}
+    >
+      <Tab.Screen name="Home" component={Main} />
+      <Tab.Screen name="Upload" component={Upload} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
+  return (
+    <Stack.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="Tabs"
+            component={TabScreen}
+            options={{headerShown: false}}
+          />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
+    </Stack.Navigator>
+  );
+};
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <StackScreen />
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
