e66e111d6f743fa23af431ae36edf6e3ce934e80
Anish Maharjan <anishm@metropolia.fi>
2023-02-22 13:03:12 +0200

    comments are being displayed in single page and add comment is also working

--- components/SingleComment.js
+++ components/SingleComment.js
@@ -1,10 +1,31 @@
-import {Text} from '@rneui/themed';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ListItem} from '@rneui/themed';
 import PropTypes from 'prop-types';
+import {useEffect, useState} from 'react';
+import {useUser} from '../hooks/ApiHooks';
 
 const SingleComment = ({singleComment}) => {
   console.log(singleComment);
+  const [user, setUser] = useState({});
+  const {getUserById} = useUser();
 
-  return <Text> {singleComment.comment}</Text>;
+  const getUser = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    const user = await getUserById(singleComment.user_id, token);
+    setUser(user);
+    console.log(user);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <ListItem>
+      <ListItem.Title>{user.username}</ListItem.Title>
+      <ListItem.Subtitle>{singleComment.comment}</ListItem.Subtitle>
+    </ListItem>
+  );
 };
 
 SingleComment.propTypes = {
--- views/Home.js
+++ views/Home.js
@@ -1,10 +1,23 @@
-import {StyleSheet, SafeAreaView, Platform} from 'react-native';
+import {StyleSheet, SafeAreaView, Platform, View} from 'react-native';
 import List from '../components/List';
 import PropTypes from 'prop-types';
+import {Card} from '@rneui/themed';
 
 const Home = ({navigation}) => {
   return (
-    <SafeAreaView style={styles.contanier}>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginStart: 10,
+        }}
+      >
+        <Card.Title style={{fontSize: 22, color: 'darkgreen'}}>
+          OmaGarden
+        </Card.Title>
+      </View>
+      <Card.Divider />
       <List navigation={navigation} />
     </SafeAreaView>
   );
