75543878c5d0062bcd9bfbaf8a9d2c52761b2b0d
Anish Maharjan <anishm@metropolia.fi>
2023-02-23 10:26:51 +0200

    added icons for search in tab navigator and added search page

---
+++ components/SingleUser.js
@@ -0,0 +1,63 @@
+import {Avatar, Card, ListItem} from '@rneui/themed';
+import {useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
+import PropTypes from 'prop-types';
+
+const SingleUser = ({singleUser}) => {
+  const user = singleUser.item;
+  const [avatar, setAvatar] = useState('');
+  const {getFilesByTag} = useTag();
+
+  // Loading the avatar of the owner of the post
+  const loadAvatar = async () => {
+    try {
+      setAvatar('');
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      setAvatar(avatarArray.pop().filename);
+    } catch (error) {
+      console.log('load Avatar', error);
+    }
+  };
+  useEffect(() => {
+    loadAvatar();
+  }, [user]);
+
+  return (
+    <Card>
+      <ListItem containerStyle={styles.avatar}>
+        {avatar ? (
+          <Avatar source={{uri: uploadsUrl + avatar}} size={40} rounded />
+        ) : (
+          <Avatar
+            source={{uri: 'https://placekitten.com/g/200/300'}}
+            size={40}
+            rounded
+          />
+        )}
+
+        <ListItem.Content>
+          <ListItem.Title> {user.username}</ListItem.Title>
+        </ListItem.Content>
+      </ListItem>
+    </Card>
+  );
+};
+
+SingleUser.propTypes = {
+  singleUser: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  Card: {
+    margin: 0,
+    padding: 0,
+  },
+  avatar: {
+    margin: 0,
+    padding: 0,
+  },
+});
+
+export default SingleUser;
