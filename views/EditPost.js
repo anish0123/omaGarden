04eb6c76a019c6cd5eea1d441c385292f42b1103
anish0123 <anishmhn@hotmail.com>
2023-03-09 10:34:05 +0200

    fixed ui in edit post

--- views/EditPost.js
+++ views/EditPost.js
@@ -1,14 +1,32 @@
 import EditPostForm from '../components/EditPostForm';
 import PropTypes from 'prop-types';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
 // This view is created for user change the details for the post/media that user has uploaded.
 const EditPost = ({route, navigation}) => {
   return (
-    <EditPostForm
-      item={route.params[0]}
-      navigation={navigation}
-      owner={route.params[1]}
-    />
+    <SafeAreaView>
+      <TouchableOpacity onPress={() => Keyboard.dismiss()} activeOpacity={1}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+          <ScrollView>
+            <EditPostForm
+              item={route.params[0]}
+              navigation={navigation}
+              owner={route.params[1]}
+            />
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
